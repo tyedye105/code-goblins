a01956f383e9163b6bb060b0b317b8a7f4805f66
Marty Kovach <tyedye105@yahoo.com>
2016-11-03 00:47:33 -0700

    add logic for the player or enemy to score a critical hit (double thier attack value) on a good dice roll, roll to hit thier target, or completly miss on a bad dice roll. Apply the dice roll logic to enemy attack when the player defends.

--- js/combatscripts.js
+++ js/combatscripts.js
@@ -1,3 +1,4 @@
+//Object used to keep track of player and enemy stats.
 function Combat(hp, attack, defense, roll) {
   this.hp = hp;
   this.attack= attack;
@@ -5,33 +6,49 @@ function Combat(hp, attack, defense, roll) {
   this.roll = roll;
 }
 
-
+//Used to determine hit and misses in the comat methods blelow.
 Combat.prototype.diceRoll = function (roll) {
   this.roll = Math.floor(Math.random() * (20 - 1)) +1;
 }
-
+// When the player chooses to attack
 Combat.prototype.playerAtk = function (hp, attack, roll) {
-  if (this.roll >= 19 && this.roll >= 4 ) {
+  if (this.roll > 18)  {
     goblin.hp = goblin.hp - (this.attack * 2);
-  }
-  else if (this.roll <= 3) {
+    alert("a critical hit!");
+  } else if (this.roll <= 19 &&  this.roll >= 4) {
     goblin.hp = goblin.hp - this.attack;
-  }
-  else {
-    alert("your attack misses the goblin")
+  } else if (this.roll <= 3) {
+  alert("your attack misses");
   }
 }
-
+//The enemy will fight back
 Combat.prototype.enemyAtk = function (hp, attack, roll) {
-  player1.hp = player1.hp - this.attack;
+  if (goblin.hp <= 0) {
+  } else if (this.roll > 18) {
+      player1.hp = player1.hp - (this.attack * 2);
+        alert(" The goblin scorese a critical hit!")
+  } else if (this.roll <= 18 &&  this.roll >= 4) {
+      player1.hp = player1.hp - this.attack;
+  }  else if (this.roll <= 3) {
+  alert("the goblin's attack misses");
+  }
 }
+//Instead of attacking, they could choose to defend and take reduced damage, enemy will still attack.
 Combat.prototype.playerDefend = function (hp, attack, defense, roll) {
-    player1.hp = player1.hp - (this.attack - player1.defense, roll);
+  if (this.roll > 18) {
+      player1.hp = player1.hp - ((this.attack * 2) - player1.defense);
+        alert(" The goblin scorese a critical hit!")
+  } else if (this.roll <= 18 &&  this.roll >= 4) {
+      player1.hp = player1.hp - ((this.attack - player1.defense));
+  }  else if (this.roll <= 3) {
+  alert("the goblin's attack misses");
+  }
 }
-Combat.prototype.enemyDefend = function (hp, attack, defense, roll) {
-  goblin.hp = goblin.hp - (this.attack - goblin.defense)
-}
-
+//  Don't have a use for this one yet, will most likely will be used for the Code Goblin King.
+// Combat.prototype.enemyDefend = function (hp, attack, defense, roll) {
+//   goblin.hp = goblin.hp - (this.attack - goblin.defense)
+// }
+//Combat ends when either the player, or enemy has 0, or negative hp.
 var victoryOrDeath = function(hp) {
   if (goblin.hp <= 0) {
     alert("The player has slain the goblin!");
@@ -47,30 +64,31 @@ var victoryOrDeath = function(hp) {
 
 
 
-
+//for the sake of the test these are global objects.
 var player1 = new Combat(10, 3, 2);
 var goblin = new Combat(9, 3, 2);
 
 $(document).ready(function() {
+  $("span#currentEhp").text(goblin.hp);
+  $("span#currentPlhp").text(player1.hp);
   $("button#attack").click(function(event){
     event.preventDefault();
     player1.diceRoll();
-    alert(player1.roll)
+    alert("p " + player1.roll)
     goblin.diceRoll();
-    alert(goblin.roll);
+    alert("g " +goblin.roll);
     player1.playerAtk();
     goblin.enemyAtk();
     victoryOrDeath();
   });
-
-
-
-
     $("button#defend").click(function(event){
       event.preventDefault();
-      alert(player1.hp);
+      player1.diceRoll();
+      alert("p " + player1.roll)
+      goblin.diceRoll();
+      alert("g " +goblin.roll);
       goblin.playerDefend();
       victoryOrDeath();
 
-    });
   });
+});
