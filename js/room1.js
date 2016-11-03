43267c3a4f307fceb96f3d34bbb9d41d5e83c7d8
jin camou <jincamou@gmail.com>
2016-11-02 21:21:29 -0700

    add html, js, css for room 1

---
+++ js/room1.js
@@ -0,0 +1,24 @@
+//business logic
+
+function Player() {
+  this.name;
+}
+
+function Door() {
+  this.door;
+}
+
+
+
+//user logic
+$(document).ready(function() {
+  $("form#terminal").submit(function(event){
+    event.preventDefault();
+  var playerInput
+
+
+
+
+
+  });
+});
