d020584e5022dd2007b435a875b5a36c115cdc77
Marty Kovach <tyedye105@yahoo.com>
2016-11-02 23:50:49 -0700

    add working d20.

--- js/combatscripts.js
+++ js/combatscripts.js
@@ -1,28 +1,41 @@
-function Combat(hp, attack, defense) {
+function Combat(hp, attack, defense, roll) {
   this.hp = hp;
   this.attack= attack;
   this.defense= defense;
+  this.roll = roll;
 }
 
 
-
-Combat.prototype.playerAtk= function (hp, attack) {
-  goblin.hp = goblin.hp - this.attack;
+Combat.prototype.diceRoll = function (roll) {
+  this.roll = Math.floor(Math.random() * (20 - 1)) +1;
 }
-Combat.prototype.enemyAtk= function (hp, attack) {
+
+Combat.prototype.playerAtk = function (hp, attack, roll) {
+  if (this.roll >= 19 && this.roll >= 4 ) {
+    goblin.hp = goblin.hp - (this.attack * 2);
+  }
+  else if (this.roll <= 3) {
+    goblin.hp = goblin.hp - this.attack;
+  }
+  else {
+    alert("your attack misses the goblin")
+  }
+}
+
+Combat.prototype.enemyAtk = function (hp, attack, roll) {
   player1.hp = player1.hp - this.attack;
 }
-Combat.prototype.playerDefend= function (hp, attack, defense) {
-    player1.hp = player1.hp - (this.attack - player1.defense);
+Combat.prototype.playerDefend = function (hp, attack, defense, roll) {
+    player1.hp = player1.hp - (this.attack - player1.defense, roll);
 }
-Combat.prototype.enemyDefend= function (hp, attack, defense) {
+Combat.prototype.enemyDefend = function (hp, attack, defense, roll) {
   goblin.hp = goblin.hp - (this.attack - goblin.defense)
 }
 
 var victoryOrDeath = function(hp) {
-  if (goblin.hp === 0) {
+  if (goblin.hp <= 0) {
     alert("The player has slain the goblin!");
-  } else if (player1.hp == 0) {
+  } else if (player1.hp <= 0) {
     alert("The goblin has slain the player!");
   }
   $("span#currentEhp").text(goblin.hp);
@@ -32,13 +45,19 @@ var victoryOrDeath = function(hp) {
 
 
 
+
+
+
 var player1 = new Combat(10, 3, 2);
 var goblin = new Combat(9, 3, 2);
-
 
 $(document).ready(function() {
   $("button#attack").click(function(event){
     event.preventDefault();
+    player1.diceRoll();
+    alert(player1.roll)
+    goblin.diceRoll();
+    alert(goblin.roll);
     player1.playerAtk();
     goblin.enemyAtk();
     victoryOrDeath();
