758c4332a837f11d7884ba70d74ccfa90f093cc8
sailor winkelman <sailorwinkelman@gmail.com>
2016-11-02 20:17:48 -0700

    Experiment with scrollTo in user-scripts. change sizes and positions in index.html. add room1 div.

--- js/user-scripts.js
+++ js/user-scripts.js
@@ -11,6 +11,16 @@ $(document).ready(function(){
     event.preventDefault();
     player1.name = $("#inputname").val();
     $("#room1").show();
+
+
     $("#name").text(player1.name);
+
+    /*this code can be used to find the position of an element*/
+    var elemRect = $("#room1").position();
+        offset   = elemRect.top - bodyRect.top;
+
+       console.log('Element is ' + elemRect.top + elemRect.bottom + elemRect.left +' vertical pixels from <body>');
   });
+
+
 });
