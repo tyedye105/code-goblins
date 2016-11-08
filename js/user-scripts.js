d10d46288e7b96bfeb3bac97d4d4c43f9700bdc6
jin camou <jincamou@gmail.com>
2016-11-07 20:17:41 -0800

    add front end logic to user-scripts.js

--- js/user-scripts.js
+++ js/user-scripts.js
@@ -2,47 +2,34 @@
 
 function Player() {
   this.name;
+  this.input = [];
 }
 
+Player.prototype.findDoor = function() {
+  if (this.input === "cd door") {
+    alert ("this text");
+  } else if (this.input === "ls -a") {
+    alert ("all files listed here");
+  } else {
+    alert ("find the door and 3 files that belong inside");
+  };
+};
 
 //user logic
-
-// create player when document is ready
 $(document).ready(function(){
   var player1 = new Player();
-
-// when name form submitted, set player name to input, show room1, input name next to $
   $("form#username").submit(function(event){
     event.preventDefault();
     player1.name = $("#inputname").val();
-    $("#room1").show();
-    $(".name").text(player1.name);
-    $("#player-stats").show();
-// when "start" button pressed, screen scroll into view
-    var room1 = document.getElementById("room1");
-    room1.scrollIntoView({behavior: "smooth"});
+    $("#screen").show();
+    $("#name").text(player1.name);
   });
-
-  $("form#terminal").submit(function(event){
-    event.preventDefault();
-    $("#intro").show();
+  $("form#terminal").submit(function(event) {
+    player1.name = $("form#terminal").val();
+    alert(player1.name);
+    player1.input = $("input#terminal-input").val();
+    alert(player1.input);
+    player1.findDoor();
   });
-
-  $("#back1").click(function(event){
-    event.preventDefault();
-    var welcome = document.getElementById("welcome");
-    welcome.scrollIntoView();
-  });
-
-
-
-
-
-  /*this code can be used to find the position of an element
-  var elemRect = $("#room1").position();
-      offset   = elemRect.top - bodyRect.top;
-
-     console.log('Element is ' + elemRect.top + elemRect.bottom + elemRect.left +' vertical pixels from <body>');*/
-
 
 });
